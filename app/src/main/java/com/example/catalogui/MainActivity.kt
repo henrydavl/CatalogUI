fdddd37926b1b4fec15d204315aa4f40d9dd75bf
Henry David Lie <ibm.henry>
2023-06-07 15:44:29 +0700

    add jitpack

--- app/src/main/java/com/example/catalogui/MainActivity.kt
+++ app/src/main/java/com/example/catalogui/MainActivity.kt
@@ -2,12 +2,16 @@ package com.example.catalogui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.catalogui.databinding.ActivityMainBinding
 import com.example.core.ui.util.Constant
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        print(Constant.masSony)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvTest.text = Constant.masSony
     }
 }
